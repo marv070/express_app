4bcb0dca90e101597c2a2c923d2bccc5bea480eb
Marvin Laucher <marv070@gmail.com>
2017-08-27 17:10:57 -0400

    add login page  for user log in.

--- app/routes.js
+++ app/routes.js
@@ -4,11 +4,25 @@ module.exports = function(app, passport) {
     res.render('index.ejs');
   });
 
+  app.get('/login', function(req , res){
+    res.render('login.ejs', { message: req.flash('loginMessage')});
+  });
+
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash: true
+  }));
+
+  app.get('/profile', isLoggedIn, function(req, res){
+    res.render('profile.ejs', {user: req.user});
+  })
+
   app.get('/signup', function(req, res) {
     res.render('signup.ejs', { message: req.flash('signupMessage') });
   });
 
-  app.post('/signup', passport.authenticate('local-signup',{
+  app.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/',
     failureRedirect: '/signup',
     failureFlash: true
@@ -25,5 +39,32 @@ module.exports = function(app, passport) {
     });
     res.send('Success!');
   })
+};
+
+function isLoggedIn(req,res,next) {
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/login');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
