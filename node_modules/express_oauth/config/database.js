f449046f9a1a830d275fa14d285a1c30282e011c
Marvin Laucher <marv070@gmail.com>
2017-08-28 16:11:31 -0400

    update package json for deploy to include path to github

---
+++ node_modules/express_oauth/config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'url'  : 'mongodb://Marv070:Pa141360@cluster0-shard-00-00-rfbq0.mongodb.net:27017,cluster0-shard-00-01-rfbq0.mongodb.net:27017,cluster0-shard-00-02-rfbq0.mongodb.net:27017/node-app?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+}
