96bbb2c74c123e3230934b0f24b44aa00262e26c
Marvin Laucher <marv070@gmail.com>
2017-08-29 15:44:50 -0400

    update port for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
