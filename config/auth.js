195aa9a0e01e70b015d85a1f44449b61ee43f49f
Marvin Laucher <marv070@gmail.com>
2017-08-30 15:02:55 -0400

    updated bcrypt

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'facebookAuth': {
     'clientID' : '173232393222581',
     'clientSecret': '90dda0343ff71f17eac4987c700b4e83',
-    'callbackURL': 'my-node-express-app.herokuapp.com/auth/facebook/callback'
+    'callbackURL': 'http://my-node-express-app.herokuapp.com/auth/facebook/callback'
   }
 }
