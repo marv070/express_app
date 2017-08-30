60f5148b3562d9957a4a9b7763bba95a71c58611
Marvin Laucher <marv070@gmail.com>
2017-08-30 09:10:05 -0400

    update return link

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'facebookAuth': {
     'clientID' : '173232393222581',
     'clientSecret': '90dda0343ff71f17eac4987c700b4e83',
-    'callbackURL': 'localhost:5000/auth/facebook/callback'
+    'callbackURL': 'my-node-express-app.herokuapp.com/auth/facebook/callback'
   }
 }
