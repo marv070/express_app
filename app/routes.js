b549758b8dd32beab99a05f357bf7d9283784d28
Marvin Laucher <marv070@gmail.com>
2017-08-29 20:41:40 -0400

    update for facebook callback

--- app/routes.js
+++ app/routes.js
@@ -67,4 +67,3 @@ function isLoggedIn(req, res, next) {
 
 
 
-
--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'facebookAuth' : {
     'clientID' : '173232393222581',
     'clientSecret' : '90dda0343ff71f17eac4987c700b4e83',
-    'callbackURL' : 'https://my-node-express-app.herokuapp.com/auth/facebook/callback'
+    'callbackURL' : 'http://localhost:5000/auth/facebook/callback'
   }
 }
