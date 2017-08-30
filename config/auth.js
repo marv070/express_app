0eef25a450693fc2d798165d55f62c0c08534e5c
Marvin Laucher <marv070@gmail.com>
2017-08-30 09:07:29 -0400

    update facebook callback for heroku route.

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
-  'facebookAuth' : {
+  'facebookAuth': {
     'clientID' : '173232393222581',
-    'clientSecret' : '90dda0343ff71f17eac4987c700b4e83',
-    'callbackURL' : 'http://localhost:5000/auth/facebook/callback'
+    'clientSecret': '90dda0343ff71f17eac4987c700b4e83',
+    'callbackURL': 'localhost:5000/auth/facebook/callback'
   }
 }
