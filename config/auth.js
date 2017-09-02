6c32833608c6c0220c9d2847f5bc095b3d673008
Marvin Laucher <marv070@gmail.com>
2017-09-02 09:43:03 -0400

    update facebook redirect

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
   'facebookAuth' : {
     'clientID': '173232393222581',
     'clientSecret': '90dda0343ff71f17eac4987c700b4e83',
-    'callbackURL': 'http://my-node-express-app.herokuapp.com/auth/facebook/callback'
+    'callbackURL': 'https://my-node-express-app.herokuapp.com/auth/facebook/callback'
     // 'callbackURL': 'http://localhost:5000/auth/facebook/callback'
   },
 
