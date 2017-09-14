7c1f23a9fb9e8aacbfd15e592b957caa8b9ef9e9
Marvin Laucher <marv070@gmail.com>
2017-09-13 20:40:57 -0400

    test that board is array of integers 1 thru 9, had to use expect instead of assert dur to assertion error.this was per stack overflow. use .to.eql when comparing equal object.

--- app/models/test.js
+++ app/models/test.js
@@ -1,5 +1,6 @@
 var chai = require('chai');
 var assert = chai.assert;
+var expect = chai.expect;
 
 var ttt = require("./board.js");
 
@@ -12,7 +13,7 @@ describe('current_playerIs', function() {
 });
 
 describe('current_playerIs', function() {
-  it('current player is o after change player function', function() {
+  it('will have current player o after change player function', function() {
     ttt.current_player = "x"
     assert.equal("o",ttt.change_player(ttt.current_player));
   });
@@ -20,9 +21,19 @@ describe('current_playerIs', function() {
 });
 
 describe('current_playerIs', function() {
-  it('current player is x after change player function', function() {
+  it('will have current player x after change player function', function() {
     ttt.current_player = "o"
     assert.equal("x",ttt.change_player(ttt.current_player));
+  });
+
+});
+
+describe('board array', function() {
+  it('will have board  array of intergers 1 thru 9', function() {
+    ttt.board_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    // assert.equal([1, 2, 3, 4, 5, 6, 7, 8, 9],ttt.board_array);
+    //You want to use deep if you're trying to compare objects: per stack overflow after assert gave assertion error
+    expect([1,2,3,4,5,6,7,8,9]).to.eql(ttt.board_array);
   });
 
 });
